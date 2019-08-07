331e572f8925ab504abedfb61ec6151ee587cbc0
Vinicius Oliveira <vicevini@live.com>
2019-08-07 10:13:36 -0300

    Tá  respondendo

--- frontend/frontend/src/pages/login.js
+++ frontend/frontend/src/pages/login.js
@@ -1,15 +1,25 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import './login.css'
 
 import logo from '../assets/logo.svg';
 
 export default function Login(){
+  const [username, setUsername] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    console.log(username); 
+  }
+
   return (
     <div className="login-container">
-      <form>
-      <img src = { logo} alt="tindev"/>
+      <form onSubmit={handleSubmit}>
+      <img src = {logo} alt="tindev"/>
       <input
       placeholder="Digite seu usuário do Github"
+      value={username}
+      onChange={e => setUsername(e.target.value)}
       />
       <button type="submit">Enviar</button>
       </form>
